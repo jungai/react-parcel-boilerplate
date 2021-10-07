04591f880ed80a7e181d09ae5e29a7cf4ee27062
jungai <juworaphol125@gmail.com>
2021-10-07 18:59:05 +0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const App = (): React.ReactElement => {
+	return (
+		<div>
+			<h1>Hello Parcel - React (ts)</h1>
+		</div>
+	);
+};
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import { App } from './App';
+
+const app = document.getElementById('app');
+ReactDOM.render(<App />, app);
